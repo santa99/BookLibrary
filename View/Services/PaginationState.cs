4898089c28471b1dec43918e1624fb4d28e0ec73
Marek Blanar <marek.blanar@sowhat.sk>
2024-05-23 13:01:05 +0200

    Partial fix for pagination. Missing total count from service.

--- View/Services/PaginationState.cs
+++ View/Services/PaginationState.cs
@@ -2,13 +2,47 @@
 
 public class PaginationState
 {
+    /// <summary>
+    /// Number of element per page.
+    /// </summary>
     public int Count { get; private set; } = 1;
 
+    /// <summary>
+    /// Page number.
+    /// </summary>
     public int Page { get; private set; } = 1;
 
+    /// <summary>
+    /// Starting element number.
+    /// </summary>
     public int StartWindow { get; private set; }
 
+    /// <summary>
+    /// Ending element number.
+    /// </summary>
     public int EndWindow { get; private set; }
+    
+    /// <summary>
+    /// Actual returned count per page.
+    /// </summary>
+    public int Available
+    {
+        set
+        {
+            IsPrevAvailable = value >= 0;
+            IsNextAvailable = value >= Count;
+        }
+    }
+
+    /// <summary>
+    /// Call this method to test whether next page is available.
+    /// </summary>
+    public bool IsNextAvailable { get; private set; }
+    
+    /// <summary>
+    /// Call this method to test whether prev page is available.
+    /// </summary>
+    public bool IsPrevAvailable { get; private set; }
 
     public PaginationState()
     {
